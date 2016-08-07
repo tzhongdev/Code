20aa2d787fcacf4fac00ebee93df05d9622aca74
Thomas <thzhdv@gmail.com>
2016-08-07 18:59:44 +0200

    Changed swipe functionality

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-// $.mobile.autoInitializePage = false;  //to stop loading text
+$.mobile.autoInitializePage = false;  //to stop loading text
 
-// /* Adds a swipe functionality to the boostrap carousel*/
+/* Adds a swipe functionality to the boostrap carousel*/
 
 
-//  $(document).ready(function() {
-//             $("#myCarousel").swiperight(function() {
-//                 $("#myCarousel").carousel('prev');
-//             });
-//             $("#myCarousel").swipeleft(function() {
-//                 $("#myCarousel").carousel('next');
-//             });
-// });
+ $(document).ready(function() {
+            $("#myCarousel").swiperight(function() {
+                $("#myCarousel").carousel('prev');
+            });
+            $("#myCarousel").swipeleft(function() {
+                $("#myCarousel").carousel('next');
+            });
+});
 
